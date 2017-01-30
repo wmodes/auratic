437e46c1fdeeeff109df227c5333381f33294b6c
Wes Modes <wmodes@gmail.com>
2017-01-29 20:31:41 -0800

    Added duration to db

--- master/rfid_object_db.py
+++ master/rfid_object_db.py
@@ -16,17 +16,20 @@ object_db = {
     "default": {
         "id": 0, 
         "category": "unknown", 
-        "title": "Mystery Object", 
+        "title": "Mystery Object",
+        "duration": 10,
         "video": "NaW43C6_Nc0"},
     "02:52:70:48:48:49:48:48:65:54:54:51:51:03:3": {
         "id": 1, 
         "category": "dishes", 
         "title": "whiskey cup", 
+        "duration": 10,
         "video": "64xeIVQK7Vs"},
     "02:52:70:48:48:49:48:52:68:56:68:57:70:03:3": {
         "id": 2, 
         "category": "housewares", 
         "title": "candle", 
+        "duration": 30,
         "video": "baadWmJKOjM"}
     }
 
