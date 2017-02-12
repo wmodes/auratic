31c0d59644c2a25f846d748572ab01a3441d53ae
Wes Modes <wmodes@gmail.com>
2017-02-12 15:33:03 -0800

    Allow keyword selection:

--- master/video/video.py
+++ master/video/video.py
@@ -28,7 +28,8 @@ inter_video_delay = 0.75
 transition_film_list = []
 content_film_list = []
 loop_film_list = []
-
+# create a dictionary of lists
+content_film_dict = {}
 
 #
 # Preparatory
@@ -45,8 +46,18 @@ def create_film_lists():
                 loop_film_list.append(film)
 
 
+def create_content_dict():
+    for film in content_film_list:
+        if 'trigger' in film:
+            trigger = film['trigger']
+            if trigger not in content_film_dict:
+                content_film_dict[trigger] = [film]
+            else:
+                content_film_dict[trigger].append(film)
+
 def main():
     create_film_lists()
+    create_content_dict()
     try:
         loop_film = choice(loop_film_list)
         loop = videothread.VideoThread([loop_film], debug=1)
@@ -57,15 +68,14 @@ def main():
             next_film = raw_input("Next video (0-%i or 'q' to quit): " % max_content)
             if ('q' in next_film):
                 break
-            try:
-                next_film = int(next_film)
-            except:
-                continue
-            if (next_film < 0 or next_film > max_content):
+            if (str.isdigit(next_film) and int(next_film) >= 0 and int(next_film) <= max_content):
+                content_film = content_film_list[int(next_film)]
+            elif (next_film in content_film_dict):
+                content_film = choice(content_film_dict[next_film])
+            else:
                 continue
             trans1_film = choice(transition_film_list)
             trans2_film = choice(transition_film_list)
-            content_film = content_film_list[next_film]
             loop = videothread.VideoThread([trans1_film, content_film, trans2_film], debug=1)
 
     except KeyboardInterrupt:
