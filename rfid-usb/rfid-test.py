a7de634e2de50b11f1b81f6dfa43c66696d1b388
Wes Modes <wmodes@gmail.com>
2017-02-25 21:33:57 -0800

    testing new usb reader

--- rfid-usb/rfid-test.py
+++ rfid-usb/rfid-test.py
@@ -9,9 +9,15 @@ keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXX
 dev = InputDevice(DEVICE)
 
 while True:
-   r,w,x = select([dev], [], [])
-   print "\nNew event.\n"
-   for event in dev.read():
+    r,w,x = select([dev], [], [])
+    #print "\nNew event.\n"
+    id = ""
+    key = ""
+    while (key != 'X'):
+        event = dev.read_one()
         if event.type==1 and event.value==1:
-                print( keys[ event.code ] )
+            key = keys[ event.code ]
+            print key
+            id += "%02d:" % int(key)
+    print id
 
