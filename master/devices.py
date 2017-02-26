ec49c98d0f540b7f011f9ffac10e1046d5f271f5
Wes Modes <wmodes@gmail.com>
2017-02-25 22:32:02 -0800

    testing new usb reader

--- master/devices.py
+++ master/devices.py
@@ -15,6 +15,7 @@ from rfid_object_db import *
 from time import sleep, time
 import threading
 from evdev import InputDevice
+from select import select
 
 from common import *
 
@@ -24,7 +25,7 @@ DEBUG = 2
 USB_PORT_PREFIX = "/dev/ttyUSB"
 MAX_USB_PORTS = 12
 RFID_SEND_COUNT = 3
-RFID_LENGTH = 41
+RFID_LENGTH = 29
 MAX_RETRIES = 20
 RETRY_DELAY = 0.5
 SERIAL_TIMEOUT = 0.5
@@ -40,6 +41,16 @@ RSP_HANDSHAKE = "hello!"
 # id's
 ID_RFID = "id:rfid"
 ID_CHART = "id:chart"
+
+scancodes = {
+    # Scancode: ASCIICode
+    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
+    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r',
+    20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
+    30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
+    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
+    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+}
 
 #
 # Globals
@@ -76,6 +87,9 @@ devices = {'rfid': {'key':      'rfid',
            }
 
 assigned_ports = []
+
+# a place to store our rfid as we receive it
+rfid_in = ""
 
 # timers
 chart_timer = ""
@@ -293,38 +307,63 @@ def stop_chart():
 def listen_and_report():
     """Do our main loop actions, particularly listening to the
     RFID reader and triggering actions"""
-    result = None
+    result = None 
+    rfid_good = None   
     try:
         update("Listening for RFID")
         rfid_device = devices['rfid']['handle']
-        # do we have data on the input buffer waiting
-        if rfid_device.in_waiting > 0:
-            # if we send the same rfid multiple times
-            #   in theory they should all be the same,
-            #   but in practice we are sometimes missing bytes.
-            #   Thus we send it multiple times to guard against data loss
-            rfid_good = ""
-            count = 0
-            # we keep looking if we have something waiting
-            #   AND we haven't exceeded our count
-            #   AND we haven't already rec'd a good rfid
-            while (rfid_device.in_waiting > 0 and count < RFID_SEND_COUNT and
-                   not rfid_good):
-                rfid_in = rfid_device.readline().strip()
-                # if the rfid has the proper length,
-                # we can trust it
-                if len(rfid_in) == RFID_LENGTH:
-                    rfid_good = rfid_in
-                    report("    Received good RFID:", rfid_in)
+        r,w,x = select([dev], [], [])
+        for event in dev.read():
+            if event.type == 1 and event.value == 1:
+                key = scancodes[event.code]
+                if key != scancodes[26]:
+                    if key.isdigit():
+                        rfid_in += "%02d:" % int(key)
                 else:
-                    report("    Received bad RFID:", rfid_in)
-            if rfid_good:
-                report("RFID found:", rfid_good)
-                result = get_rfid_data(rfid_good)
-            # clear incoming buffer in case we have stuff waiting
-            rfid_device.reset_input_buffer()
-            rfid_device.flushInput()
-            report("Continue listening for RFID")
+                    rfid_in = id[0:-1]
+                    # if the rfid has the proper length,
+                    # we can trust it
+                    if len(rfid_in) == RFID_LENGTH:
+                        rfid_good = rfid_in
+                        report("    Received good RFID:", rfid_in)
+                    else:
+                        report("    Received bad RFID:", rfid_in)
+                    rfid_in = ""
+        if rfid_good:
+            report("RFID found:", rfid_good)
+            result = get_rfid_data(rfid_good)
+        # clear incoming buffer in case we have stuff waiting
+        # rfid_device.reset_input_buffer()
+        # rfid_device.flushInput()
+        report("Continue listening for RFID")
+        # # do we have data on the input buffer waiting
+        # if rfid_device.in_waiting > 0:
+        #     # if we send the same rfid multiple times
+        #     #   in theory they should all be the same,
+        #     #   but in practice we are sometimes missing bytes.
+        #     #   Thus we send it multiple times to guard against data loss
+        #     rfid_good = ""
+        #     count = 0
+        #     # we keep looking if we have something waiting
+        #     #   AND we haven't exceeded our count
+        #     #   AND we haven't already rec'd a good rfid
+        #     while (rfid_device.in_waiting > 0 and count < RFID_SEND_COUNT and
+        #            not rfid_good):
+        #         rfid_in = rfid_device.readline().strip()
+        #         # if the rfid has the proper length,
+        #         # we can trust it
+        #         if len(rfid_in) == RFID_LENGTH:
+        #             rfid_good = rfid_in
+        #             report("    Received good RFID:", rfid_in)
+        #         else:
+        #             report("    Received bad RFID:", rfid_in)
+        #     if rfid_good:
+        #         report("RFID found:", rfid_good)
+        #         result = get_rfid_data(rfid_good)
+        #     # clear incoming buffer in case we have stuff waiting
+        #     rfid_device.reset_input_buffer()
+        #     rfid_device.flushInput()
+        #     report("Continue listening for RFID")
     except IOError:
         update("WARNING: Lost RFID device")
     return(result)
